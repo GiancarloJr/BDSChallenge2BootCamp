6708a78c709ec342dc630d2910416040b8fd2e3f
GiancarloJr <giancarlo456@hotmail.com>
2022-12-04 17:59:11 -0300

    Movie Resources, Services and Repository

--- backend/src/main/java/com/devsuperior/movieflix/service/UserService.java
+++ backend/src/main/java/com/devsuperior/movieflix/service/UserService.java
@@ -3,7 +3,6 @@ package com.devsuperior.movieflix.service;
 import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
-import com.devsuperior.movieflix.service.exceptions.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 
 @Service
